266e04605c5d6c03b3165326fff549bf48130d09
lijian <hustlijian@hust.edu.cn>
2014-03-03 16:28:15 +0800

    add the command line paraser

--- mysql_test.py
+++ mysql_test.py
@@ -5,18 +5,25 @@ date: 2014-03-01
 author: lijian <hustlijian@gmail.com>
 description:
     input data to mysql database table
+    query the key, and count the QPS
 '''
-
-import MySQLdb
 import random
 import time
 import sys
+import getopt
+import MySQLdb
 
 HOST = 'localhost'
 USER = 'root'
 PASSWD = 'root'
 DB = 'dbtest'
 DBTABLE = 'testtable'
+HELP = '''usage: mysql_test.py [-h] [-i count] [-q times]
+desc.:
+    [-h] --- get usage help
+    [-i count] --- insert count of number into database
+    [-q times] --- query the database the given times and get the QPS
+'''
 
 MAX = 10000  # random max number
 TIME = 10000 # query times
@@ -34,41 +41,53 @@ except MySQLdb.ERROR, e:
 cursor = conn.cursor()
 
 def insert():
+    print 'insert count :%s'%MAX
     # insert data into table 
     for i in range(MAX):
         command = 'insert into %s (num) values (%d)' %(DBTABLE,random.randint(0,MAX))
         cursor.execute(command)
     conn.commit()
     
-def show():
-    # show result
-    command = 'SELECT * FROM %s' %DBTABLE
-    cursor.execute(command)
-    result = cursor.fetchall()
-    for record in result:
-        print record[0] , "-->", record[1]
-    
 def query():
-    # query the num in database
-    QUERY = random.randint(0,MAX)
-    command = 'SELECT * FROM %s WHERE id=%d'%(DBTABLE, QUERY)
-    cursor.execute(command)
-    #result = cursor.fetchall()
-
-def main():
-    #insert()
-
+    print 'query times: %d'%TIME
     start_time = int(round(time.time()*1000))
     for i in range(TIME):
-        query()
+        # query the num in database
+        QUERY = random.randint(0,MAX)
+        command = 'SELECT * FROM %s WHERE id=%d'%(DBTABLE, QUERY)
+        cursor.execute(command)
+        #result = cursor.fetchall()
+        
     end_time  = int(round(time.time()*1000))
     #print 'query time: %d milliseconds' %(end_time-start_time)
     print '[------MySQL QPS Test------]'
     print 'QPS: %d query/seconds'%(TIME*1000/(end_time-start_time))
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hq:i:")
+    except getopt.GetoptError:
+        print(HELP)
+        sys.exit(1)
+    if not opts:
+        print HELP
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print HELP
+            sys.exit()
+        elif opt == '-i':
+            global MAX
+            MAX = int(arg)
+            insert()
+        elif opt == '-q':
+            global TIME 
+            TIME = int(arg)
+            query()
 
     # close connection
     cursor.close()
     conn.close()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
--- redis_test.py
+++ redis_test.py
@@ -4,28 +4,36 @@
 date: 2014-03-01
 author: lijian <hustlijian@gmail.com>
 description:
-    input data to mysql database table
+    input data to redis database 
+    query the key, and count the QPS
 '''
 
-import redis
 import random
 import time
 import sys
+import getopt
+import redis
 
 HOST = 'localhost'
 PORT = 6379
 DB = 0
+HELP = '''usage: redis_test.py [-h] [-i count] [-q times]
+desc.:
+    [-h] --- get usage help
+    [-i count] --- insert count of number into database
+    [-q times] --- query the database the given times and get the QPS
+'''
 
 MAX = 10000 # inseart max number
 TIME = 10000 # query times
 QUERY = MAX/2   # the key to query,init here, random choosed in use place
 
 # connect the database
-
 r = redis.StrictRedis(host=HOST, port=PORT, db=DB)
 
 def insert():
     # insert data into database
+    print 'insert count :%s'%MAX
     for i in range(MAX):
         key = str(i)
         value = str(random.randint(0,MAX)) 
@@ -33,22 +41,40 @@ def insert():
         r.set(key, value)
     
 def query():
-    # query the num in database
-    QUERY = random.randint(0, MAX)
-    key = str(QUERY)
-    value = r.get(key)
-    #print 'key:%s value:%s'%(key, value)
-
-def main():
-    #insert()
-
+    print 'query times: %d'%TIME
     start_time = int(round(time.time()*1000))
     for i in range(TIME):
-        query()
+        # query the num in database
+        QUERY = random.randint(0, MAX)
+        key = str(QUERY)
+        value = r.get(key)
+        #print 'key:%s value:%s'%(key, value)
     end_time  = int(round(time.time()*1000))
     #print 'query time: %d milliseconds' %(end_time-start_time)
     print '[------Redis QPS Test------]'
     print 'QPS: %d query/seconds'%(TIME*1000/(end_time-start_time))
 
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hq:i:")
+    except getopt.GetoptError:
+        print(HELP)
+        sys.exit(1)
+    if not opts:
+        print HELP
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print HELP
+            sys.exit()
+        elif opt == '-i':
+            global MAX
+            MAX = int(arg)
+            insert()
+        elif opt == '-q':
+            global TIME 
+            TIME = int(arg)
+            query()
+
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
