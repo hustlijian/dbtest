b55e8935611c7704d992de957f67f84e760677f2
lijian <hustlijian@hust.edu.cn>
2014-03-09 11:16:39 +0800

    add sqlite test

---
+++ sqlite_test.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+'''
+date: 2014-03-01
+author: lijian <hustlijian@gmail.com>
+description:
+    input data to sqlite database table
+    query the key, and count the QPS
+'''
+import random
+import time
+import sys
+import getopt
+import sqlite3
+
+DB = 'dbtest.db'
+DBTABLE = 'testtable'
+HELP = '''usage: sqlite_test.py [-h] [-i count] [-q times]
+desc.:
+    [-h] --- get usage help
+    [-i count] --- insert count of number into database
+    [-q times] --- query the database the given times and get the QPS
+'''
+
+MAX = 10000  # random max number
+TIME = 10000 # query times
+QUERY = 1   # the key to query, init here, and random in use place
+
+# connect the database
+try:
+    conn = sqlite3.connect(DB)
+except Exception, e:
+    print 'Error %s'%(e)
+    exit(1)
+
+# create cursor
+cursor = conn.cursor()
+
+def insert():
+    print 'insert count :%s'%MAX
+    # create table
+    command = 'CREATE TABLE IF NOT EXISTS %s (id integer primary key autoincrement,num integer)'%(DBTABLE)
+    cursor.execute(command)
+    # insert data into table 
+    for i in range(MAX):
+        command = 'insert into %s (num) values (%d)' %(DBTABLE,random.randint(0,MAX))
+        cursor.execute(command)
+    conn.commit()
+    
+def query():
+    print 'query times: %d'%TIME
+    start_time = int(round(time.time()*1000))
+    for i in range(TIME):
+        # query the num in database
+        QUERY = random.randint(0,MAX)
+        command = 'SELECT * FROM %s WHERE id=%d'%(DBTABLE, QUERY)
+        cursor.execute(command)
+        #result = cursor.fetchall()
+        
+    end_time  = int(round(time.time()*1000))
+    #print 'query time: %d milliseconds' %(end_time-start_time)
+    print '[------Sqlite QPS Test------]'
+    print 'QPS: %d query/seconds'%(TIME*1000/(end_time-start_time))
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hq:i:")
+    except getopt.GetoptError:
+        print(HELP)
+        sys.exit(1)
+    if not opts:
+        print HELP
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print HELP
+            sys.exit()
+        elif opt == '-i':
+            global MAX
+            MAX = int(arg)
+            insert()
+        elif opt == '-q':
+            global TIME 
+            TIME = int(arg)
+            query()
+
+    # close connection
+    cursor.close()
+    conn.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
